20f10e9cb01325d2c5de2176b35db1f5cb02bb17
Vaibhav Dhiman <vars7899@gmail.com>
2022-12-03 14:49:11 -0800

    Update: brightness up on qrcode screen

--- client/views/TicketScreen.js
+++ client/views/TicketScreen.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import {
   ScrollView,
   StyleSheet,
@@ -7,9 +8,21 @@ import {
 import TicketCard from "../components/Ticket/TicketCard";
 import CurrentTabHeader from "../components/CurrentTabHeader/CurrentTabHeader";
 import { theme } from "../global";
+import * as Brightness from "expo-brightness";
 
 const TicketScreen = () => {
   const { width } = useWindowDimensions();
+  // increase device brightness when ticket component mounts
+  useEffect(() => {
+    (async () => {
+      const { status } = await Brightness.requestPermissionsAsync();
+      console.log(initialBrightness);
+      if (initialBrightness && status === "granted") {
+        Brightness.setBrightnessAsync(1.75);
+      }
+    })();
+    return () => Brightness.setBrightnessAsync(0.1);
+  }, []);
   return (
     <View style={[styles.container, { width }]}>
       <CurrentTabHeader currentTab="Thor: Love and Thunder" />
